1445ac2e070f1228425b3552e997f2c6da1ffe77
Jacob Warrer <jawar19@student.sdu.dk>
2025-02-15 17:50:28 +0100

    add preliminary utils and locationHandler
    Small steps to save requests locally, and the boilerplate for some location handler class

--- src/py_rejseplan/__init__.py
+++ src/py_rejseplan/__init__.py
@@ -5,7 +5,8 @@ from . import constants
 from . import dataclasses
 from . import locationhandler
 from . import departureboard
+from . import exceptions
 
 # logging.getLogger('pyRejseplan').addHandler(logging.NullHandler())
 
-__all__ = [constants, dataclasses, locationhandler, departureboard]
+__all__ = [constants, dataclasses, locationhandler, departureboard, exceptions]
