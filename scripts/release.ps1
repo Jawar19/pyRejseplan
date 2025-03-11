bdec6193b17991794414bd0d2a02e8764a14438b
Jacob Warrer <jawar19@student.sdu.dk>
2025-03-11 20:10:30 +0100

    reenable git commit and automatic upload functionality

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -49,31 +49,31 @@ if ($version -match '^(\d+)\.(\d+)\.(\d+)?$') {
 }
 
 # Commit the changes and create a new git tag
-# git add .
-# git commit -m $commitMessage
+git add .
+git commit -m $commitMessage
 git tag -a "v$version" -m $commitMessage
-# git push origin --tags
+git push origin --tags
 
 # Build the distribution files using python -m build
 python -m build
 
-# $files = Get-ChildItem -Path "dist" -Filter "*$newVersion*" -File
-# if ($files.Count -eq 0) {
-#     Write-Host "No files found matching version $newVersion in the dist folder."
-#     exit 1
-# }
-# # Upload the distribution files using Twine
-# twine upload $files
+$files = Get-ChildItem -Path "dist" -Filter "*$newVersion*" -File
+if ($files.Count -eq 0) {
+    Write-Host "No files found matching version $newVersion in the dist folder."
+    exit 1
+}
+# Upload the distribution files using Twine
+twine upload $files
 
-# # Check if GitHub CLI is available
-# if (-not (Get-Command gh -ErrorAction SilentlyContinue)) {
-#     Write-Host "GitHub CLI (gh) is not installed or not available in the PATH."
-#     Write-Host "You can install it using winget with the following command:"
-#     Write-Host "winget install --id GitHub.cli"
-#     exit 1
-# }
+# Check if GitHub CLI is available
+if (-not (Get-Command gh -ErrorAction SilentlyContinue)) {
+    Write-Host "GitHub CLI (gh) is not installed or not available in the PATH."
+    Write-Host "You can install it using winget with the following command:"
+    Write-Host "winget install --id GitHub.cli"
+    exit 1
+}
 
-# # Create a new GitHub release and upload the files
-# $releaseNotes = "Release version $newVersion\n$commitMessage"
-# gh release create $newVersion $files --title "Release $newVersion" --notes $releaseNotes --latest
+# Create a new GitHub release and upload the files
+$releaseNotes = "Release version $newVersion\n$commitMessage"
+gh release create $newVersion $files --title "Release $newVersion" --notes $releaseNotes --latest
 
