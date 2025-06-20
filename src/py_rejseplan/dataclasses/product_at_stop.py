9f39536f805baed5e182efceed6aba160a03caa5
Jacob Warrer <jawar19@student.sdu.dk>
2025-06-20 16:59:16 +0200

    fix dataclass type and added missing attributes and elements

--- src/py_rejseplan/dataclasses/product_at_stop.py
+++ src/py_rejseplan/dataclasses/product_at_stop.py
@@ -7,37 +7,60 @@ class ProductAtStop(
     BaseXmlModel,
     tag='ProductAtStop',
     ns="",
-    nsmap=constants.NSMAP
+    nsmap=constants.NSMAP,
+    search_mode='unordered',
 ):
     """ProductAtStop class for parsing XML data from the Rejseplanen API.
     This class is used to represent the product at stop data returned by the API.
     It extends the BaseXmlModel from pydantic_xml to provide XML parsing capabilities.
     """
-
-    name: Optional[str] = attr()
-    internalName: Optional[str] = attr()
+    # attributes
+    name: Optional[str] = attr(default=str)
+    internalName: Optional[str] = attr(default=str)
     addName: Optional[str] = attr(default="", tag='addName')
     displayNumber: Optional[str] = attr(default="", tag='displayNumber')
-    num: Optional[int] = attr()
-    line: Optional[str] = attr()
-    lineId: Optional[str] = attr()
+    num: Optional[int] = attr(default=str)
+    line: Optional[str] = attr(default=str)
+    lineId: Optional[str] = attr(default=str)
     lineHidden: bool = attr(default=False, tag='lineHidden')
-    catOut: Optional[str] = attr()
-    catIn: Optional[str] = attr()
-    catCode: Optional[str] = attr()
-    cls: Optional[str] = attr()
-    catOutS: Optional[str] = attr()
-    catOutL: Optional[str] = attr()
-    operatorCode: Optional[str] = attr()
-    operator: Optional[str] = attr()
-    admin: Optional[str] = attr()
+    catOut: Optional[str] = attr(default=str)
+    catIn: Optional[str] = attr(default=str)
+    catCode: Optional[str] = attr(default=str)
+    cls: Optional[str] = attr(default=str)
+    catOutS: Optional[str] = attr(default=str)
+    catOutL: Optional[str] = attr(default=str)
+    operatorCode: Optional[str] = attr(default=str)
+    operator: Optional[str] = attr(default=str)
+    admin: Optional[str] = attr(default=str)
     routeIdxFrom: int = attr(default=-1, tag='routeIdxFrom')
     routeIdxTo: int = attr(default=-1, tag='routeIdxTo')
-    matchId: Optional[str] = attr()
+    matchId: Optional[str] = attr(default=str)
+
+    #Customer specific attributes
+    tarGr: Optional[str] = attr(default=str, tag='tarGr')
+    surcharge: Optional[str] = attr(default=str, tag='surcharge')
+    outCtrl: Optional[str] = attr(default=str, tag='outCtrl')
+    locTraffic: Optional[str] = attr(default=str, tag='locTraffic')
+    shipTraffic: Optional[str] = attr(default=str, tag='shipTraffic')
 
     icon: dict[str, str] = element(
         default_factory=dict,
         tag='icon'
+    )
+
+    status: dict[str, str] = element(
+        default_factory=dict,
+        tag='status'
+    )
+
+    fromLocation: dict[str, str] = element(
+        default_factory=dict,
+        tag='fromLocation'
+    )
+
+    toLocation: dict[str, str] = element(
+        default_factory=dict,
+        tag='toLocation'
     )
 
     operatorInfo: dict[str, str] = element(
@@ -45,6 +68,30 @@ class ProductAtStop(
         tag='operatorInfo'
     )
 
+    Note: list[dict[str, str]] = element(
+        default_factory=list,
+        tag='Note'
+    )
+
+    Message: list[dict[str, str]] = element(
+        default_factory=list,
+        tag='Message'
+    )
+    LineInfo: dict[str, str] = element(
+        default_factory=dict,
+        tag='LineInfo'
+    )
+
+
+
+# <xs:element name="icon" type="IconType" minOccurs="0" maxOccurs="1"/>
+# <xs:element name="status" type="ProductStatusType" minOccurs="0" maxOccurs="1"/>
+# <xs:element name="fromLocation" type="StopType" minOccurs="0" maxOccurs="1"/>
+# <xs:element name="toLocation" type="StopType" minOccurs="0" maxOccurs="1"/>
+# <xs:element name="operatorInfo" type="OperatorType" minOccurs="0" maxOccurs="1"/>
+# <xs:element name="Note" type="Note" minOccurs="0" maxOccurs="unbounded"/>
+# <xs:element name="Message" type="Message" minOccurs="0" maxOccurs="unbounded"/>
+# <xs:element name="LineInfo" type="LineType" minOccurs="0" maxOccurs="1"/>
 
 # - name: str, optional
 # - internalName: str, optional
@@ -66,3 +113,11 @@ class ProductAtStop(
 # - routeIdxFrom: int, default="-1"
 # - routeIdxTo: int, default="-1"
 # - matchId: str, optional
+
+# <xs:attribute name="tarGr" type="xs:string" use="optional"/>
+# <xs:attribute name="surcharge" type="xs:string" use="optional"/>
+# <xs:attribute name="outCtrl" type="xs:string" use="optional"/>
+# <xs:attribute name="locTraffic" type="xs:string" use="optional"/>
+# <xs:attribute name="shipTraffic" type="xs:string" use="optional"/>
+
+
