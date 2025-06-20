1e3c697a21a142ef465ef505eb336e9d83f97426
Jacob Warrer <jawar19@student.sdu.dk>
2025-06-20 17:00:13 +0200

    add stop class in dataclasses

--- src/py_rejseplan/dataclasses/enums.py
+++ src/py_rejseplan/dataclasses/enums.py
@@ -1,15 +1,7 @@
 from enum import Enum
 
 class PrognosisType(str, Enum):
-    """Types of prognosis for departure times.
-    
-    Indicates how the prognosis was determined:
-    - PROGNOSED: Reported from external provider as future prognosis
-    - MANUAL: Manually entered by external provider
-    - REPORTED: Reported delay from previously passed stations
-    - CORRECTED: System-corrected to ensure journey consistency
-    - CALCULATED: System-calculated for upcoming or gap-filling stations
-    """
+    """Types of prognosis for departure times."""
     PROGNOSED = "PROGNOSED"
     MANUAL = "MANUAL"
     REPORTED = "REPORTED"
@@ -17,18 +9,35 @@ class PrognosisType(str, Enum):
     CALCULATED = "CALCULATED"
 
 class DepartureType(str, Enum):
-    """The attribute type specifies the type of departs location. 
-    Valid values are: 
-        ST (stop/station), 
-        ADR (address), 
-        POI (point of interest), 
-        CRD (coordinate), 
-        MCP (mode change point) 
-        HL (hailing point).
-    """
+    """The attribute type specifies the type of departs location."""
     ST = "ST"
     ADR = "ADR"
     POI = "POI"
     CRD = "CRD"
     MCP = "MCP"
     HL = "HL"
+
+class RealtimeDataSourceType(str, Enum):
+    """Realtime data source types."""
+    DEFAULT = "DEFAULT"           # Default source (undefined)
+    VDV = "VDV"
+    HIM = "HIM"
+    HRC = "HRC"
+    SIRI = "SIRI"
+    UIC = "UIC"
+    HRX = "HRX"
+    GTFS = "GTFS"
+    FIS = "FIS"
+    DDS = "DDS"                  # Datendrehscheiben
+    PAISA = "PAISA"              # PA-ISA
+    FE = "FE"                    # FahrtenEditor
+    BLACKLIST = "BLACKLIST"      # List of blacklisted trains
+    ARAMIS = "ARAMIS"            # ARAMIS data source
+    RTABO2 = "RTABO2"            # RTABO2 data source
+
+class JourneyStatusType(str, Enum):
+    """Contains the status of the journey."""
+    P = "P"  # Planned: A planned journey. This is also the default value.
+    R = "R"  # Replacement: The journey was added as a replacement for a planned journey.
+    A = "A"  # Additional: The journey is an additional journey to the planned journeys.
+    S = "S"  # Special: This is a special journey. The exact definition which journeys are considered special is up to the customer.
